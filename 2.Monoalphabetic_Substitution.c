c44bb96c265b23b8a2bbd19ecb1737a11cd5d5b6
Sai krishna <107762536+Saikrishna1504@users.noreply.github.com>
2024-09-23 21:08:07 +0530

    Update 2.Monoalphabetic_Substitution.c

--- 2.Monoalphabetic_Substitution.c
+++ 2.Monoalphabetic_Substitution.c
@@ -2,27 +2,22 @@
 #include <stdlib.h>
 #include <ctype.h>
 #define ALPHABET_SIZE 26
-char encrypt(char letter, char key[]) {
-    if (isalpha(letter)) {
-        char base = isupper(letter) ? 'A' : 'a';
-        return key[letter - base];
-    }
-    return letter;
-}
-void monoalphabeticEncrypt(char message[], char key[]) {
-    int i = 0;
-    while (message[i] != '\0') {
-        message[i] = encrypt(message[i], key);
-        i++;
-    }
-}
+
 int main() {
     char message[1000];
-    char key[ALPHABET_SIZE + 1] = "ZYXWVUTSRQPONMLKJIHGFEDCBA"; // Example key (reverse alphabet)
+    char key[ALPHABET_SIZE + 1] = "ZYXWVUTSRQPONMLKJIHGFEDCBA"; 
     printf("Enter the message to encrypt: ");
     fgets(message, sizeof(message), stdin);
-    printf("Original message: %s\n", message);
-    monoalphabeticEncrypt(message, key);
-    printf("Encrypted message: %s\n", message);
+    
+    printf("Original message: %s", message); 
+    
+    for (int i = 0; message[i] != '\0'; i++) {
+        if (isalpha(message[i])) {
+            char base = isupper(message[i]) ? 'A' : 'a';
+            message[i] = key[message[i] - base];
+        }
+    }
+    
+    printf("Encrypted message: %s", message);
     return 0;
 }

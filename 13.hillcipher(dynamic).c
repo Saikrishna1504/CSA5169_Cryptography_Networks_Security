85042813c00d0919e159e7d6643d7f04a6813874
Sai krishna <107762536+Saikrishna1504@users.noreply.github.com>
2024-09-25 21:24:53 +0530

    Update 13.hillcipher(dynamic).c

--- 13.hillcipher(dynamic).c
+++ 13.hillcipher(dynamic).c
@@ -1,85 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-#define MAX_LEN 100
-
-// Function to convert a character to its numeric representation (0-25)
-int charToNum(char c) {
-    if (isupper(c)) {
-        return c - 'A';
-    } else if (islower(c)) {
-        return c - 'a';
-    }
-    return -1; // Return -1 for non-alphabet characters
-}
-
-// Function to convert a number (0-25) to its corresponding uppercase character
-char numToChar(int num) {
-    return num + 'A';
-}
-
-// Function to encrypt plaintext using Hill cipher
-void encryptHill(char *text, int *keyMatrix, int keySize) {
-    int i, j, k;
-    int textLen = strlen(text);
-    int encrypted[MAX_LEN] = {0};
-
-    for (i = 0; i < textLen; i += keySize) {
-        for (j = 0; j < keySize; j++) {
-            int sum = 0;
-            for (k = 0; k < keySize; k++) {
-                sum += keyMatrix[j * keySize + k] * charToNum(text[i + k]);
-            }
-            encrypted[i + j] = sum % 26;
-        }
-    }
-
-    for (i = 0; i < textLen; i++) {
-        text[i] = numToChar(encrypted[i]);
-    }
-    text[textLen] = '\0'; // Null-terminate the encrypted text
-}
+#define MOD 26
 
 int main() {
-    char plaintext[MAX_LEN];
-    int keySize;
+    int plaintext1[2] = {1, 3};    
+    int ciphertext1[2] = {2, 3};   
+    int plaintext2[2] = {1, 2};    
+    int ciphertext2[2] = {1, 4};   
 
-    printf("Enter the plaintext: ");
-    fgets(plaintext, sizeof(plaintext), stdin);
-    plaintext[strcspn(plaintext, "\n")] = '\0'; // Remove newline character
+    int key[2][2];    
 
-    printf("Enter the size of the key matrix: ");
-    scanf("%d", &keySize);
+    key[0][0] = (ciphertext1[0] - ciphertext2[0] + MOD) % MOD;    
+    key[0][1] = (ciphertext1[1] - ciphertext2[1] + MOD) % MOD;
 
-    if (keySize <= 0 || keySize > MAX_LEN) {
-        printf("Invalid key size.\n");
-        return 1;
-    }
+    key[1][0] = (plaintext1[0] * key[0][0] + plaintext1[1] * key[0][1]) % MOD;
+    key[1][1] = (plaintext2[0] * key[0][0] + plaintext2[1] * key[0][1]) % MOD;
 
-    int i, j;
-    int keyMatrix[MAX_LEN * MAX_LEN];
+    if (key[1][0] < 0) key[1][0] += MOD;
+    if (key[1][1] < 0) key[1][1] += MOD;
 
-    printf("Enter the key matrix (row by row):\n");
-    for (i = 0; i < keySize; i++) {
-        for (j = 0; j < keySize; j++) {
-            scanf("%d", &keyMatrix[i * keySize + j]);
-        }
-    }
-
-    int textLen = strlen(plaintext);
-    int padding = keySize - (textLen % keySize);
-
-    if (padding < keySize) {
-        for (i = 0; i < padding; i++) {
-            plaintext[textLen + i] = 'X'; // Padding character
-        }
-        plaintext[textLen + padding] = '\0';
-    }
-
-    encryptHill(plaintext, keyMatrix, keySize);
-    printf("Encrypted text: %s\n", plaintext);
+    printf("Recovered key:\n");
+    printf("%d %d\n", key[0][0], key[0][1]);
+    printf("%d %d\n", key[1][0], key[1][1]);
 
     return 0;
 }
